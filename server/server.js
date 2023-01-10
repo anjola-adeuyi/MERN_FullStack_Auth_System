a78e6a84d399184b06e45803e03466bbcb490a2d
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2023-01-10 23:12:01 +0100

    fixing ENV dtatbase connection

--- server/server.js
+++ server/server.js
@@ -12,28 +12,27 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.disable('x-powered-by'); // less hackers know about our stack
 
-
 const port = 8080;
 
 /** HTTP GET Request */
 app.get('/', (req, res) => {
-    res.status(201).json("Home GET Request");
+  res.status(201).json('Home GET Request');
 });
 
-
 /** api routes */
-app.use('/api', router)
+app.use('/api', router);
 
 /** start server only when we have valid connection */
-connect().then(() => {
+connect()
+  .then(() => {
     try {
-        app.listen(port, () => {
-            console.log(`Server connected to http://localhost:${port}`);
-        })
+      app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+      });
     } catch (error) {
-        console.log('Cannot connect to the server')
+      console.log('Cannot connect to the server');
     }
-}).catch(error => {
-    console.log("Invalid database connection...!");
-})
-
+  })
+  .catch((error) => {
+    console.log('Invalid database connection...!');
+  });
